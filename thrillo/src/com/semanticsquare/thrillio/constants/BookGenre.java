36ad4ccecfe07e6f991f1febd8160b912d310931
Kartik Kaurav <kkaurav@cisco.com>
2018-03-22 23:22:49 -0700

    saving text files

---
+++ thrillo/src/com/semanticsquare/thrillio/constants/BookGenre.java
@@ -0,0 +1,17 @@
+package com.semanticsquare.thrillio.constants;
+
+public class BookGenre {
+	private BookGenre (){}; 
+	public static final String ART = "Art";
+	public static final String BIOGRAAPHY = "BIOGRAAPHY";
+	public static final String CHILDREN = "CHILDREN";
+	public static final String FICTION = "FICTION";
+	public static final String HISTORY = "HISTORY";
+	public static final String MISTRY = "HISTORY";
+	public static final String PHILOSOPHY = "PHILOSOPHY";
+	public static final String RELIGION = "RELIGION";
+	public static final String ROMANCE = "ROMANCE";
+	public static final String TECHNOLOGY= "TECHNOLOGY";
+	
+
+}
