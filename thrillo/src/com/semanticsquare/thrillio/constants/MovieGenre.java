fceb5b433f3a1b4fcca89edf58e7d2738ef137ad
kartikkaurav16 <kartikkaurav@gmail.com>
2018-03-27 22:17:02 -0700

    adding new const

--- thrillo/src/com/semanticsquare/thrillio/constants/MovieGenre.java
+++ thrillo/src/com/semanticsquare/thrillio/constants/MovieGenre.java
@@ -1,5 +1,15 @@
 package com.semanticsquare.thrillio.constants;
 
 public class MovieGenre {
+	private MovieGenre () {}; 
+	
+	public static final String DRAMA = "DRAMA";
+	public static final String ROMANCE = "ROMANCE";
+	public static final String ACTION = "ACTION";
+	public static final String FICTION = "FICTION";
+	public static final String HORROR = "HORROR";
+	public static final String THRILLER = "THRILLER";
+	public static final String SPORTS = "SPORTS";
+	public static final String RELIGIOUS = "RELIGIOUS";
 
 }
