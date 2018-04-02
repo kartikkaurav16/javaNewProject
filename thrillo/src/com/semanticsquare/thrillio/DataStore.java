e777f87b84c8cdb397b8066d695151b164ff1fe9
kartikkaurav16 <kartikkaurav@gmail.com>
2018-04-01 17:40:09 -0700

    data store

--- thrillo/src/com/semanticsquare/thrillio/DataStore.java
+++ thrillo/src/com/semanticsquare/thrillio/DataStore.java
@@ -1,7 +1,8 @@
 package com.semanticsquare.thrillio;
 
 		
-	import com.semanticsquare.thrillio.constants.Gender;
+	import com.semanticsquare.thrillio.constants.BookGenre;
+import com.semanticsquare.thrillio.constants.Gender;
 import com.semanticsquare.thrillio.constants.MovieGenre;
 import com.semanticsquare.thrillio.constants.UserType;
 import com.semanticsquare.thrillio.entities.Bookmark;
@@ -48,9 +49,13 @@ bookmark[0][1] = BookmarkManager.getInstance().createWeblink("http://tomcat.apac
 
 	 }
 	 
-   private static void loadBook() {
-		 bookmark[1][0]= BookmarkManager.getInstance().createBook(1854, publiser, author, id, title, genre, amazonRating)
-				 4000	Walden	1854	Wilder Publications	Henry David Thoreau	Philosophy	4.3
+   private static void loadBooks() {
+bookmark[1][0]= BookmarkManager.getInstance().createBook(1854, "Wilder Publications", new String[] {"Henry David", "Henry David"}, 4000, "Walden", BookGenre.PHILOSOPHY, 4.3);
+bookmark[1][1]= BookmarkManager.getInstance().createBook(1993, "Dover Publications", new String[] {"Ralph Waldo ", "Emerson"}, 4001, "Self-Reliance and Other Essays", BookGenre.PHILOSOPHY, 4.5);
+bookmark[1][2]= BookmarkManager.getInstance().createBook(1988, "Touchstone", new String[] {"Eichler Watson"}, 4002, "Light From Many Lamps", BookGenre.PHILOSOPHY, 4.7);
+bookmark[1][3]= BookmarkManager.getInstance().createBook(2004, "O'Reilly Media", new String[] {"Bert Bates", "Kathy Sierra", "Elisabeth Robson"}, 4003, "Head First Design Patterns", BookGenre.TECHNOLOGY, 5.0);
+bookmark[1][4]= BookmarkManager.getInstance().createBook(2007, "Prentice Hall", new String[] {"Joshua Bloch"}, 4004, "Effective Java Programming Language Guide", BookGenre.TECHNOLOGY, 4.9);
+
 	 }
    
    private static void loadMovies() {
@@ -58,11 +63,7 @@ bookmark[2][0] = BookmarkManager.getInstance().createMovie(3000, "Citizen Kane",
 bookmark[2][1] = BookmarkManager.getInstance().createMovie(3001, "The Grapes of Wrath",MovieGenre.CLASSICS, 1940 ,new String[] {"Henry Fonda", "Jane Darwell"}, new String[] {"John Ford"}, 8.2,"");
 bookmark[2][2] = BookmarkManager.getInstance().createMovie(3002, "A Touch of Greatness",MovieGenre.ROMANCE, 2004 ,new String[] {"Albert Cullum"}, new String[] {"Leslie Sullivan"}, 7.3,"");
 bookmark[2][3] = BookmarkManager.getInstance().createMovie(3003, "The Big Bang Theory",MovieGenre.DRAMA, 2007 ,new String[] {"Kaley Cuoco", "Kaley Cuoco"}, new String[] {"Kaley Cuoco","Bill Prady"}, 8.7,"");
-bookmark[2][4] = BookmarkManager.getInstance().createMovie(3000, "Citizen Kane",MovieGenre.CLASSICS, 1941 ,new String[] {"Orson", "Welles"}, new String[] {"Orson Welles","Joseph Cotten"}, 8.5,"");
-			   
-long id,String title,String genre,int releaseYear ,String[] director, String[] cast,double imdbRating
-
-3003	The Big Bang Theory	2007	Kaley Cuoco,Jim Parsons	Chuck Lorre,Bill Prady	TV Shows	8.7	unknown
+bookmark[2][4] = BookmarkManager.getInstance().createMovie(3004, "dil chatha hai ",MovieGenre.CLASSICS, 2000 ,new String[] {"Fearhan Akthar", "Zoya"}, new String[] {"Amir","saif","Aksahy Khanna", "prity Zinta"}, 8.5,"");
 
    }
 	 
